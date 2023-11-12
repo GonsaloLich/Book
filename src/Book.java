ad58b452db9f8025225b13ec832e18d5377fc0ae
GonsaloLich <kantsaris@gmail.com>
2023-11-12 14:53:06 +0300

    дз готово

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private Author author;
     private String title;
@@ -31,5 +33,21 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+    public String toString() {
+        return "Автор " + this.author + ", Название книги " + this.title + ", год выпуска" + this.year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(author, book.author) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, title, year);
     }
 }
